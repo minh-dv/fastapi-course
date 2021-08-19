915ba5b1661b0d5a57b96c5d546d9a65e78c13ee
DESKTOP-MVV38K8\MINH <minh.dv98@gmail.com>
2021-08-19 15:49:56 +0700

    Connect DB Postgres

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine  # new
+from db.base_class import Base  # new
 
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+def create_tables():  # new
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    create_tables()  # new
+    return app
+
+
+app = start_application()
 
 
 @app.get("/")
 def hello_api():
-    return {"detail": "Hello World!"}
+    return {"detail":"Hello World!"}
